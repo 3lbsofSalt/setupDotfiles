4fb64afb52d0e301062802bd9cef2b4e9b57bc6e
madram <madramsdell@gmail.com>
2021-01-30 15:12:43 -0700

    Added fehbg

--- setup.sh
+++ setup.sh
@@ -1,5 +1,13 @@
 #!/bin/bash
 cd $(dirname "${BASH_SOURCE[0]}")
+
 full_path="$(realpath $0)"
 cur_dir="$(dirname $full_path)"
-ln -s $cur_dir/.vimrc ~/.vimrc
+
+sudo pacman -S --needed vim
+ln -sf $cur_dir/.vimrc ~/.vimrc
+
+mkdir -p ~/photos/wallpapers
+ln -sf $cur_dir/backgrounds/hipsterPineNeedles.jpg ~/photos/wallpapers/hipsterPineNeedles.jpg
+sudo pacman -S --needed feh
+ln -sf $cur_dir/.fehbg ~/.fehbg
