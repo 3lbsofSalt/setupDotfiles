a2d1107c7ebe19f1f82b79c59f6e717ff44c5520
madram <madramsdell@gmail.com>
2021-02-02 08:56:04 -0700

    Added i3 and polybar as well as support for installing aur packages

--- setup.sh
+++ setup.sh
@@ -4,10 +4,28 @@ cd $(dirname "${BASH_SOURCE[0]}")
 full_path="$(realpath $0)"
 cur_dir="$(dirname $full_path)"
 
-sudo pacman -S --needed vim
+#Setup Vim
+sudo pacman -S --needed --noconfirm vim
 ln -sf $cur_dir/.vimrc ~/.vimrc
 
+#Set up wallpaper
 mkdir -p ~/photos/wallpapers
 ln -sf $cur_dir/backgrounds/hipsterPineNeedles.jpg ~/photos/wallpapers/hipsterPineNeedles.jpg
-sudo pacman -S --needed feh
+sudo pacman -S --needed --noconfirm feh
 ln -sf $cur_dir/.fehbg ~/.fehbg
+
+#Set up i3 and polybar
+mkdir -p ~/.config
+sudo pacman -S --needed --noconfirm i3-wm i3-gaps
+sudo pacman -S --needed --noconfirm git
+
+mkdir -p aur
+cd aur
+git clone https://aur.archlinux.org/polybar.git
+cd polybar
+makepkg -si --needed --noconfirm
+cd ../..
+
+ln -sf $cur_dir/i3 ~/.config/i3
+ln -sf $cur_dir/polybar ~/.config/polybar
+
