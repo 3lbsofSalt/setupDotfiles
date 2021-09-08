84d57ad3ca5d93fb9b7d5d12084f13c085df23ac
madram <madramsdell@gmail.com>
2021-09-08 15:13:54 -0600

    Trying to have better feh support

--- setup.sh
+++ setup.sh
@@ -10,15 +10,20 @@ ln -sf $cur_dir/.vimrc ~/.vimrc
 
 #Set up wallpaper
 mkdir -p ~/photos/wallpapers
-ln -sf $cur_dir/backgrounds/hipsterPineNeedles.jpg ~/photos/wallpapers/hipsterPineNeedles.jpg
+#ln -sf $cur_dir/backgrounds/hipsterPineNeedles.jpg ~/photos/wallpapers/hipsterPineNeedles.jpg
 sudo pacman -S --needed --noconfirm feh
-ln -sf $cur_dir/.fehbg ~/.fehbg
+feh --bg-fill $cur_dir/backgrounds/goatGlacier/2560x1440.jpg
+#ln -sf $cur_dir/.fehbg ~/.fehbg
 
 #Set up i3 and polybar
 mkdir -p ~/.config
 sudo pacman -S --needed --noconfirm i3-wm i3-gaps
 sudo pacman -S --needed --noconfirm alacritty
 sudo pacman -S --needed --noconfirm git
+
+git config --global user.name "Max Ramsdell"
+git config --global user.email madramsdell@gmail.com
+git config --global core.editor vim
 
 mkdir -p aur
 cd aur
